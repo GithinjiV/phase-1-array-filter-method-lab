7aca3d724fbdf788ac6df94f19a8dbded2c04f8c
vinnie <githinjigicheru146@gmail.com>
2024-01-31 22:56:09 +0300

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, sought){
+    return drivers.filter(possibleMatch => possibleMatch.toLowerCase() === sought.toLowerCase());
+
+}
+
+function fuzzyMatch(source, stringPartial){
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(stringPartial.toLowerCase())=== 0
+    );
+}
+
+function matchName(source, soughtName){
+    return source.filter(
+       (record) => record.name === soughtName
+    );
+}
